89703a76058d302f594c022b712ec00b52b9a754
Simone Romiti <sr1992ei@gmail.com>
2021-02-05 17:47:23 +0100

    Aggiunta funzione sconto se anno < 1980

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		public $author;
 		public $year;
 		public $plot;
+		public $discount = 0;
 
 		public function __construct($constrTitle, $constrAuthor, $constrYear, $constrPlot) {
 			$this->title = $constrTitle;
@@ -16,12 +17,22 @@
 			$this->plot = $constrPlot;
 		}
 
+		public function setDiscount($num){
+			if($num < 1980){
+				$this->discount = 40;
+			}
+		}
 	}
+
 
 	foreach ($data as $libro) {
 		$book = new Book($libro['title'], $libro['author'], $libro['year'], $libro['plot']);
 		$arrayObj[] = $book;
 	}
+
+		foreach ($arrayObj as $obj) {
+			$obj->setDiscount($obj->year);
+		}
 	
 	var_dump($arrayObj);
 
